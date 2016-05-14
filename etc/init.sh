407d122bd5694184114df5af1f40b004c4afe8fd
avzabr <avzabr@gmail.com>
2016-05-15 00:39:37 +0300

    1.9.11 Fix #1

--- etc/init.sh
+++ etc/init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo cp -r /home/box/stepic-web-tech/ /home/box/web/
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-gunicorn -b 0.0.0.0:8080 hello &
+sudo ln -sf /home/box/web/etc/hello.py /home/box/web/hello.py
+gunicorn -b 0.0.0.0:8080 hello:hello &
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
