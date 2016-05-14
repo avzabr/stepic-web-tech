5c9da3d1e1ade8773ff0ef64b89fdbd88cd53b6e
avzabr <avzabr@gmail.com>
2016-05-15 00:45:11 +0300

    1.9.11 Fix #2

--- etc/hello.py
+++ etc/hello.py
@@ -1,4 +1,4 @@
 def hello(env, start_response):
     queries = env['QUERY_STRING'].split('&')
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return '/n/r'.join(queries)
+    return '\r\n'.join(queries)
