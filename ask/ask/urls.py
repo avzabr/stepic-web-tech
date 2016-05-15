d138b8356ae79f1122fbf461463f2b7de48f12cd
avzabr <avzabr@gmail.com>
2016-05-15 13:02:46 +0300

    Urls typo fix

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^login/', test, name='login'),
     url(r'^signup/', test, name='signup'),
     url(r'^question/(?P<id>[^/]+)/?', test, name='question'),
-    url(r'^ask/', test, name='question'),
+    url(r'^ask/', test, name='ask'),
     url(r'^popular/', test, name='popular'),
     url(r'^new/', test, name='new')
 ]
