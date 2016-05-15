7fe1ddfccd71d9778c4561aa785fef5bf9e2a094
avzabr <avzabr@gmail.com>
2016-05-15 12:49:58 +0300

    2.1.11 Implementation of ask django project

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from ask.qa.views import test
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
+    url(r'^$', test),
+    url(r'^login/', test, name='login'),
+    url(r'^signup/', test, name='signup'),
+    url(r'^question/(?P<id>[^/]+)/?', test, name='question'),
+    url(r'^ask/', test, name='question'),
+    url(r'^popular/', test, name='popular'),
+    url(r'^new/', test, name='new')
 ]
--- etc/init.sh
+++ etc/init.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 sudo cp -r /home/box/stepic-web-tech/ /home/box/web/
+
+#gunicorn hello wsgi
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/hello.py /home/box/web/hello.py
 gunicorn -b 0.0.0.0:8080 hello:hello &
+
+#gunicorn django
+sudo ln -sf /home/box/web/etc/gunicorn.ask.conf /etc/gunicorn.d/ask
+sudo /etc/init.d/gunicorn restart
+
+#nginx
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
