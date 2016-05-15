dcb174e00c32a30671bec3ac53adc85a820bffa6
avzabr <avzabr@gmail.com>
2016-05-15 17:02:55 +0300

    2.1.11 gunicorn configuration fix

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
-from ask.qa.views import test
+from qa.views import test
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
     url(r'^$', test),
     url(r'^login/', test, name='login'),
     url(r'^signup/', test, name='signup'),
